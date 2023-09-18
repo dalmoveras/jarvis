d89a18778eef423c247124e2fc53b1ae6c02f672
dalmoveras <d.verasnt@gmail.com>
2023-09-18 16:36:21 -0700

    start add()

--- jarvis/src/lib.rs
+++ jarvis/src/lib.rs
@@ -1,23 +1,41 @@
+use std::{ env, fs };
+use std::path::Path;
+
 pub struct TodoList {
     pub todo: Vec<String>,
     pub todo_path: String,
-    pub backup: String,
+    //pub backup: String,
 }
 
 impl TodoList {
     pub fn new() -> Result<Self, String> { 
-        let todo = vec!();
-        let todo_path = String::new();
-        let backup = String::new();
+        //let todo = vec!();
+        let todo_path: String = match env::var("JARVIS_PATH") {
+            Ok(todo_path) => todo_path,
+            Err(_) => {
+                let home = env::var("HOME").unwrap();
+                format!("{}/.jarvis", &home)
+            }
+        };
 
-        Ok(Self{todo, backup, todo_path })
+        
+        let todo_list = OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(true)
+            .open(&todo_path)
+            .expect("Jarvis couldn't open the file");
+        
+        //let backup = String::new();
+        
+        Ok(Self{todo, todo_path })
 
     }
 
     pub fn list() {}    
-
     pub fn add() {}
     pub fn remove() {}
+    pub fn encrypt() {}
     pub fn delete() {}
     pub fn restore() {}
 
