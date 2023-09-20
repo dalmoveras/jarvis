a8b218de19e2e1ca89e63cf05c56c08a8c55e9ee
dalmoveras <d.verasnt@gmail.com>
2023-09-19 21:00:51 -0700

    fix todolist::new

--- jarvis/src/lib.rs
+++ jarvis/src/lib.rs
@@ -14,16 +14,15 @@ pub struct Task {
 }
 
 pub struct TodoList {
-    pub todo: Vec<String>,
-    pub file: File,
-    pub todo_path: String,
+    pub tasks: Vec<Task>,
+    pub jarvis_path: String,
 }
 
 impl std::fmt::Display for Task {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
-            "Owner: {}, Mission: {}, Created At: {}, Status: {}, Completed At: {:?}",
+            "\nOwner: {} | Mission: {} | Created At: {} | Status: {} | Completed At: {:?} |",
             self.owner, self.mission, self.created_at, self.status, self.completed_at
         )
     }
@@ -51,25 +50,14 @@ impl Task {
 
 impl TodoList {
     pub fn new() -> Result<Self, Error> {
-        let todo_path: String = match env::var("JARVIS_PATH") {
-            Ok(todo_path) => todo_path,
-            Err(_) => {
-                let home = env::var("HOME").unwrap();
-                let previous_todo = format!("{}/.jarvis", &home);
-                match Path::new(&previous_todo).exists() {
-                    true => previous_todo,
-                    false => format!("{}/.jarvis", &home),
-                }
-            }
-        };
-        let todo = vec![];
-        let mut file = File::create(&todo_path).unwrap();
-        write!(file, "JARVIS LOG FILE\n");
-        Ok(Self {
-            todo,
-            file,
-            todo_path,
-        })
+        let home = env::var("HOME").unwrap();
+        let jarvis_path = format!("{}/.jarvis", &home);
+        let tasks: Vec<Task> = vec![];
+        if !Path::new(&jarvis_path).exists() {
+            let mut file = File::create(&jarvis_path).unwrap();
+            write!(file, "JARVIS LOG FILE\n");
+        }
+        Ok(Self { tasks, jarvis_path })
     }
 
     pub fn add(&self, tasks: &[Task]) {
@@ -82,12 +70,11 @@ impl TodoList {
             .write(true)
             .read(true)
             .append(true)
-            .open(&self.todo_path)
+            .open(&self.jarvis_path)
             .expect("Jarvis couldn't open the file");
 
         for task in tasks {
             println!("{}", task);
-            //write!(todo_list, "{} - {:?}\n",task, timestamp);
             write!(todo_list, "{}", task);
         }
     }
@@ -108,7 +95,7 @@ mod tests {
         let todo = TodoList::new().unwrap();
         assert_eq!(
             format!("{}/.jarvis", env::var("HOME").unwrap()),
-            todo.todo_path
+            todo.jarvis_path
         );
     }
 
